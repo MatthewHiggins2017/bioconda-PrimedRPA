ebcbf7b860e16ee1bcac0f943096dbf27087ad1d
Matt H <matthew.higgins@blueridgebio.co.uk>
2020-04-05 16:57:00 +0100

    PrimedPRA Updates

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setuptools.setup(
     name="primedrpa",
 	version="1.0.0",
 	license="GPL3",
+    packages=setuptools.find_packages(),
 	long_description="RPA Primer & Probe Design Tool",
 	scripts= ['PrimedRPA'])
