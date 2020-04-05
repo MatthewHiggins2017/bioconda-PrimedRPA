d32ddcf5e0c2e7c8f26c1f3a28569652cbfc24af
Matt H <matthew.higgins@blueridgebio.co.uk>
2020-04-05 16:10:46 +0100

    PrimedRPA Bioconda

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import setuptools
 setuptools.setup(
     name="primedrpa",
 	version="1.0.0",
-	packages=["primedrpa",],
 	license="GPL3",
 	long_description="RPA Primer & Probe Design Tool",
 	scripts= ['PrimedRPA'])
