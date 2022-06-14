96179637f5cfd1f235e5957bda14f301ea9c57cb
joostvhts <joostvhts@gmail.com>
2022-06-14 11:19:58 +0200

    Remove files unnecessary for colab

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="primedrpa",
-	version="1.0.0",
-	license="GPL3",
-    packages=setuptools.find_packages(),
-	long_description="RPA Primer & Probe Design Tool",
-	scripts= ['PrimedRPA'])
